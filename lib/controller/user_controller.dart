3283acd2d56e0be784f982d9f2bb43e9577d3229
thiago <rumbleh@gmail.com>
2021-02-23 18:01:50 -0300

    Inclusão de banco de dados Firebase, Serviços e Modelos.

---
+++ lib/controller/user_controller.dart
@@ -0,0 +1,86 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:evento_app/model/user.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_database/firebase_database.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class UserController extends StatefulWidget {
+  @override
+  _UserControllerState createState() => _UserControllerState.instance;
+
+  Future<ApiResponse> adicionarParticipante(User user, GlobalKey<FormState> formKey){
+    return createState()._cadastrar(user, formKey);
+  }
+}
+
+class _UserControllerState extends State<UserController> {
+
+  String firebaseUserUid;
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  DatabaseReference databaseReference = FirebaseDatabase.instance.reference();
+
+  static _UserControllerState _instance;
+  _UserControllerState._(){}
+
+  static _UserControllerState get instance => _instance = _instance??_UserControllerState._();
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+
+  Future<ApiResponse> _cadastrar(User user, GlobalKey<FormState> formKey) async {
+    // Usuario do firebase
+    try {
+      AuthResult result = await _auth.createUserWithEmailAndPassword(
+          email: user.email, password: user.password);
+      final FirebaseUser fUser = result.user;
+
+      // Dados para atualizar o usuario
+      final userUpdateInfo = UserUpdateInfo();
+      userUpdateInfo.displayName = user.name;
+      fUser.updateProfile(userUpdateInfo);
+
+      // salvar dados no firebase
+      if (fUser != null) {
+        firebaseUserUid = fUser.uid;
+        DocumentReference refUser = Firestore.instance.collection("user")
+            .document(firebaseUserUid);
+        refUser.setData({
+          "name": user.name,
+          "email": user.email,
+          "password": user.password
+        });
+      }
+
+      formKey.currentState.save();
+      formKey.currentState.reset();
+      return ApiResponse.ok(msg: "Usuário cadastrado com sucesso");
+
+    } catch (error){
+      print(error);
+      if (error is PlatformException) {
+        print("Error code: ${error.code}");
+        return ApiResponse.ok(msg: "Erro ao criar o usuário.\n\n\Este e-mail já foi cadastrado em nosso sistema.");
+      }
+      return ApiResponse.error(msg: "Não foi possível criar um usuário.");
+    }
+    // resposta generica
+    return null;
+  }
+}
+
+class ApiResponse<T> {
+  bool ok;
+  String msg;
+  T result;
+
+  ApiResponse.ok({this.result, this.msg}){
+    ok = true;
+  }
+
+  ApiResponse.error({this.result, this.msg}){
+    ok = false;
+  }
+}
--- lib/view/signup_screen.dart
+++ lib/view/signup_screen.dart
@@ -1,3 +1,7 @@
+import 'dart:convert';
+import 'package:crypto/crypto.dart';
+import 'package:evento_app/controller/user_controller.dart';
+import 'package:evento_app/model/user.dart';
 import 'package:flutter/material.dart';
 
 class SignupScreen extends StatefulWidget {
@@ -6,14 +10,16 @@ class SignupScreen extends StatefulWidget {
 }
 
 class _SignupScreenState extends State<SignupScreen> {
-
   final TextEditingController _nome = new TextEditingController();
   final TextEditingController _email = new TextEditingController();
   final TextEditingController _senha = new TextEditingController();
+  final GlobalKey<FormState> formKey = GlobalKey<FormState>();
+  final globalKey = GlobalKey<ScaffoldState>();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      key: globalKey,
       appBar: AppBar(
         title: Text(
           "Cadastro de usuário",
@@ -23,60 +29,80 @@ class _SignupScreenState extends State<SignupScreen> {
       ),
       body: SafeArea(
           child: Padding(
-            padding: const EdgeInsets.all(50.0),
-            child: Container(
-              child: Form(
-                  child: Column(
-                    children: [
-                      Flexible(
-                        flex: 1,
-                        child: Image.asset(
-                          "assets/images/logo.png",
-                          width: 150.0,
-                          height: 150.0,
-                        ),
-                      ),
-                      Flexible(
-                        child: TextFormField(
-                          maxLength: 100,
-                          decoration: InputDecoration(hintText: "Nome"),
-                          validator: (val) => val == "" ? val : null,
-                          controller: _nome,
-                          keyboardType: TextInputType.text,
-                        ),
-                      ), Flexible(
-                        child: TextFormField(
-                          maxLength: 100,
-                          decoration: InputDecoration(hintText: "E-mail"),
-                          validator: (val) => val == "" ? val : null,
-                          controller: _email,
-                          keyboardType: TextInputType.emailAddress,
-                        ),
-                      ),
-                      Flexible(
-                        child: TextFormField(
-                          maxLength: 20,
-                          obscureText: true,
-                          decoration: InputDecoration(hintText: "Senha"),
-                          validator: (val) => val == "" ? val : null,
-                          controller: _senha,
-                          keyboardType: TextInputType.text,
-                        ),
-                      ),
-
-                      Flexible(
-                          child: FlatButton(
-                            color: Colors.deepPurple,
-                              child: Text("Cadastrar", style: TextStyle(color: Colors.white),),
-                              onPressed: () {
-                                Navigator.of(context).push(MaterialPageRoute(
-                                    builder: (BuildContext context) =>
-                                        SignupScreen()));
-                              }))
-                    ],
-                  )),
-            ),
-          )),
+        padding: const EdgeInsets.all(50.0),
+        child: Container(
+          child: Form(
+              key: formKey,
+              child: Column(
+                children: [
+                  Flexible(
+                    flex: 1,
+                    child: Image.asset(
+                      "assets/images/logo.png",
+                      width: 150.0,
+                      height: 150.0,
+                    ),
+                  ),
+                  Flexible(
+                    child: TextFormField(
+                      maxLength: 100,
+                      decoration: InputDecoration(hintText: "Nome"),
+                      validator: (val) => val == "" ? val : null,
+                      controller: _nome,
+                      keyboardType: TextInputType.text,
+                    ),
+                  ),
+                  Flexible(
+                    child: TextFormField(
+                      maxLength: 100,
+                      decoration: InputDecoration(hintText: "E-mail"),
+                      validator: (val) => val == "" ? val : null,
+                      controller: _email,
+                      keyboardType: TextInputType.emailAddress,
+                    ),
+                  ),
+                  Flexible(
+                    child: TextFormField(
+                      maxLength: 20,
+                      obscureText: true,
+                      decoration: InputDecoration(hintText: "Senha"),
+                      validator: (val) => val == "" ? val : null,
+                      controller: _senha,
+                      keyboardType: TextInputType.text,
+                    ),
+                  ),
+                  Flexible(
+                      child: FlatButton(
+                          color: Colors.deepPurple,
+                          child: Text(
+                            "Cadastrar",
+                            style: TextStyle(color: Colors.white),
+                          ),
+                          onPressed: () {
+                            _cadastraUsuario(globalKey);
+                          }))
+                ],
+              )),
+        ),
+      )),
     );
+  }
+
+  void _cadastraUsuario(GlobalKey<ScaffoldState> globalKey) async {
+    User user = User(_nome.text, _email.text, _convertToSha1(_senha.text));
+    if (formKey.currentState.validate()){
+      ApiResponse response = await UserController().adicionarParticipante(user, formKey);
+      if (response != null){
+        globalKey.currentState.showSnackBar(SnackBar(content: Text(response.msg)));
+      } else {
+        globalKey.currentState.showSnackBar(SnackBar(content: Text("Não foi possível cadastrar o usuário.")));
+      }
+    } else {
+      globalKey.currentState.showSnackBar(SnackBar(content: Text("Preencha todos os campos em branco.")));
+    }
+  }
+
+  String _convertToSha1(String text){
+    return sha1.convert(utf8.encode("eVeNtApP" + text)).toString();
   }
 }
